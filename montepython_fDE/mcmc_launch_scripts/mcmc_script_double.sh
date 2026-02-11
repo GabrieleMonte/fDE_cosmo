0d31396e6828ec12586877fb05e68419611a8056
GabrieleMonte <gabrielemontef@gmail.com>
2026-02-10 22:47:21 -0600

    more analyses

--- montepython_fDE/mcmc_launch_scripts/mcmc_script_double.sh
+++ montepython_fDE/mcmc_launch_scripts/mcmc_script_double.sh
@@ -4,8 +4,8 @@
 #SBATCH -e sbatch_logs/err/launcher.e%j       # Name of stderr error file
 #SBATCH -p development          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
-#SBATCH -c 15
-#SBATCH -n 8               # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -c 10
+#SBATCH -n 12               # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 02:00:00        # Run time (hh:mm:ss)
 #SBATCH --mail-type=all    # Send email at begin and end of job
 #SBATCH -A PHY23028       # Project/Allocation name (req'd if you have more than 1)
@@ -14,18 +14,18 @@
 
 module load launcher
 
-export OMP_NUM_THREADS=15
+export OMP_NUM_THREADS=10
 
 export MONTEPYTHON="work/09218/gab97/ls6/fDE_cosmo/montepython_fDE/MontePython.py"
 export CHAIN_DIRECTORY=/scratch/09218/gab97/chains
-export CHAIN_NAME1=desi_dr2_lcdm_v1
-export CHAIN_NAME2=desi_dr2_w0wa_v2
+export CHAIN_NAME1=desi_dr2_Qcmb_fa_v2
+export CHAIN_NAME2=desi_dr2_Qcmb_w0wa_v2
 
 export PREVIOUS_NSAMPLES=30
 export PREVIOUS_NSAMPLES2=30
 
-export NSAMPLES=300000
-export NSAMPLES2=300000
+export NSAMPLES=100000
+export NSAMPLES2=100000
 export PREVIOUS_CHAIN_FILE1=$(ls ${CHAIN_DIRECTORY}/${CHAIN_NAME1}/*${PREVIOUS_NSAMPLES}__1.txt)
 export PREVIOUS_CHAIN_FILE2=$(ls ${CHAIN_DIRECTORY}/${CHAIN_NAME2}/*${PREVIOUS_NSAMPLES2}__1.txt)
 
@@ -34,6 +34,6 @@ export JUMPING_FACTOR2=2.4
 
 export LAUNCHER_WORKDIR=/work/09218/gab97/ls6/fDE_cosmo/montepython_fDE/mcmc_launch_scripts
 
-export LAUNCHER_JOB_FILE=$LAUNCHER_WORKDIR/mcmc_launcher_2chains_4per_nocov
+export LAUNCHER_JOB_FILE=$LAUNCHER_WORKDIR/mcmc_launcher_2chains_6per_nocov
 
 ${LAUNCHER_DIR}/paramrun
